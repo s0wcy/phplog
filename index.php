f419fd0d1c82864d3817c86db4ea1f21969d066d
SaucySpray <alexandrechabeau.pro@gmail.com>
2019-02-05 17:50:23 +0100

    :tada: Init & Header

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHPlog - achabe.com</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+    <header>
+        <h1 class="header-logo">blog.achabe.com</h1>
+        <nav class="header-nav">
+            <div class="burger">
+                <span class="burger-el burger-el-1"></span>
+                <span class="burger-el burger-el-2"></span>
+                <span class="burger-el burger-el-3"></span>
+                <span class="burger-el burger-el-4"></span>
+            </div>
+            <form action="" class="search">
+                <input type="text" class="search-input">
+            </form>
+            <div class="search-btn"></div>
+        </nav>
+        <div class="header-social">
+            <a href="https://twitter.com/SaucySpray" class="social-el social-el-1"></a>
+            <a href="https://github.com/SaucySpray" class="social-el social-el-2"></a>
+            <a href="http://www.achabe.com/" class="social-el social-el-3"></a>
+        </div>
+    </header>
+
+    <div class="main">
+    
+    </div>
+
+    <script src="./js/index.js"></script>
+
+</body>
+</html>
