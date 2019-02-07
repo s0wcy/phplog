f3a230a903e3f66f78c39001f31c18dc128e4e61
SaucySpray <alexandrechabeau.pro@gmail.com>
2019-02-07 14:13:29 +0100

    :lipstick: Burger animation

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const $burger = {}
+$burger.btn = document.querySelector('.burger')
+$burger.part1 =document.querySelector('.burger-el-1')
+$burger.part2 =document.querySelector('.burger-el-2')
+$burger.part3 =document.querySelector('.burger-el-3')
+$burger.part4 =document.querySelector('.burger-el-4')
+
+$burger.btn.addEventListener('click', () => {
+    $burger.part1.classList.toggle('burger-el-1-anim')
+    $burger.part2.classList.toggle('burger-el-2-anim')
+    $burger.part3.classList.toggle('burger-el-3-anim')
+    $burger.part4.classList.toggle('burger-el-4-anim')
+})
